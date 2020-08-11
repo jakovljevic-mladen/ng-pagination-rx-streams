e09dacea0280d7c0cff28d99eec22e8fe8b7cef0
Mladen Jakovljević <jakovljevic.mladen@gmail.com>
2020-08-11 21:43:54 +0200

    Add feed refresh on every 30 seconds.

--- src/app/feed-component/feed.component.ts
+++ src/app/feed-component/feed.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Subscription, timer } from 'rxjs';
 
 import { FeedService } from '../feed-service/feed.service';
 
@@ -7,10 +9,22 @@ import { FeedService } from '../feed-service/feed.service';
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.css']
 })
-export class FeedComponent {
+export class FeedComponent implements OnInit, OnDestroy {
+
+  private subscription: Subscription;
 
   feed$ = this.feedService.feed$;
 
+  refresh$ = timer(0, 30000);
+
   constructor(private feedService: FeedService) {
+  }
+
+  ngOnInit(): void {
+    this.subscription = this.refresh$.subscribe(this.feedService.refresh$);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
--- src/app/feed-service/feed.service.ts
+++ src/app/feed-service/feed.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { BehaviorSubject, Observable } from 'rxjs';
+import { exhaustMap, share } from 'rxjs/operators';
+
 import { FakeFeedResponse } from '../models';
 
 @Injectable({
@@ -8,7 +11,13 @@ import { FakeFeedResponse } from '../models';
 })
 export class FeedService {
 
-  feed$ = this.http.get<FakeFeedResponse[]>('/feed');
+  getFeed$ = this.http.get<FakeFeedResponse[]>('/feed').pipe(share());
+
+  refresh$ = new BehaviorSubject(null);
+
+  feed$: Observable<FakeFeedResponse[]> = this.refresh$.pipe(
+    exhaustMap(() => this.getFeed$)
+  );
 
   constructor(private http: HttpClient) {
   }
