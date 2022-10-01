d65cc42ae6770a41acbfd4b22b4d516ee3f3ee69
Mladen Jakovljević <jakovljevic.mladen@gmail.com>
2022-10-01 10:18:43 +0200

    refactor: Use IntersectionObserver to listen to the scroll position
    chore(Angular): Update to Angular v14
    chore(faker): Update faker to v7

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,10 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
---
+++ src/app/asIntersectionObservable.ts
@@ -0,0 +1,13 @@
+import { Observable } from 'rxjs';
+
+export function asIntersectionObservable(target: Element, options?: IntersectionObserverInit): Observable<IntersectionObserverEntry> {
+  return new Observable(subscriber => {
+    const callback = ([entry]: IntersectionObserverEntry[]) => subscriber.next(entry);
+
+    const io = new IntersectionObserver(callback, options);
+
+    io.observe(target);
+
+    return () => io.disconnect();
+  });
+}
